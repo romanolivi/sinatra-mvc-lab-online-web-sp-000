3c98c56bac5b4056d9f192932e75b909a8c99dcf
Romano Livi <romano_livi@outlook.com>
2021-01-27 18:27:15 +0000

    Done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,26 @@
+require 'pry'
+class PigLatinizer 
+  
+  def piglatinize(string)
+    string.split.map {|word| pig_translate(word)}.join(" ")
+  end
+  
+  def pig_translate(word)
+    alphabet = ('a'..'z').to_a + ('A'..'Z').to_a
+    vowel = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+    consonant = alphabet - vowel
+    
+    if vowel.include?(word[0])
+      word = word + "way"
+    elsif consonant.include?(word[0]) && consonant.include?(word[1]) && consonant.include?(word[2])
+      word = word.slice(3..-1) + word.slice(0,3) << "ay"
+    elsif consonant.include?(word[0]) && consonant.include?(word[1])
+      word = word.slice(2..-1) + word.slice(0,2) << "ay"
+    elsif consonant.include?(word[0])
+      word = word.slice(1..-1) + word.slice(0,1) << "ay"
+    end
+    word
+  end
+  
+  
+end
